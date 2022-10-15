2c3b8b58d039140acca946e77a7f356b5aae25cd
ShishirDasNiloy <shishirdasniloy@gmail.com>
2022-10-15 11:04:13 +0600

    added: code file

---
+++ src/db/core.py
@@ -0,0 +1,36 @@
+from pydantic import AnyHttpUrl, BaseSettings, HttpUrl, validator
+from typing import Optional, Union, List, Optional
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class Settings(BaseSettings):
+    API_V_STR: str = "/api"
+    DATABASE_URL: str = os.environ.get("DATABASE_URL")
+    SECRET_KEY: str = os.environ.get("SECRET_KEY")
+    ALGORITHM: str = os.os.environ.get("ALGORITHM")
+
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7
+
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = [
+        os.environ.get("URL_ONE"),
+        os.environ.get("URL_TWO"),
+    ]
+
+    @validator("BACKEND_CORS_ORIGINS", pre = True)
+    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
+        if isinstance(v, str) and not v.startswith("["):
+            return [i.strip() for i in v.split(",")]
+        elif isinstance(v, (list, str)):
+            return v
+        raise ValueError(v)
+
+    PROJECT_NAME: str = "OverSee"
+    SENTRY_DSN: Optional[HttpUrl] = None
+
+    class Config:
+        case_sensitive = True
+
+
+settings = Settings()
