9ca747423ce13539679dbfa5779798df5c4cbfcd
ShishirDasNiloy <shishirdasniloy@gmail.com>
2022-10-22 10:52:40 +0600

    added: admin signup service

---
+++ src/services/admin.py
@@ -0,0 +1,38 @@
+from services import BaseService
+from .users import users_service
+from schemas import UserCreate, UserUpdate
+from models import User
+from repositories import admin_repo, roles_repo
+from sqlalchemy.orm import Session
+from exceptions import ServiceResult, AppException
+
+
+class Admin(BaseService[User, UserCreate, UserUpdate]):
+
+    def signup_admin(self, db: Session, data_in: UserCreate):
+
+        admin_id = roles_repo.search_name_id(db, name='admin')
+
+        admin_exist = self.repo.search_by_role_id(db, id=admin_id)
+
+        if not admin_exist:
+            sginup_data = UserCreate(
+                name=data_in.name,
+                email=data_in.email,
+                phone=data_in.phone,
+                sex=data_in.sex,
+                is_active=True,
+                password=data_in.password,
+                role_name='admin'
+            )
+
+            signup_admin = users_service.signup(
+                db, data_in=sginup_data, flush=False)
+
+            return signup_admin
+
+        return ServiceResult(AppException.ServerError('Admin exist'))
+
+
+
+admin_service = Admin(User, admin_repo)
