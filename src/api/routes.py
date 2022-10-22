83c0e8abead0c99f777899239052a4fd9659de55
ShishirDasNiloy <shishirdasniloy@gmail.com>
2022-10-22 10:49:35 +0600

    added: route for roles

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from .endpoints import users
+from .endpoints import users, roles
 
 api_router = APIRouter()
 
 api_router.include_router(users.router, prefix='', tags = ["Users"])
+api_router.include_router(roles.router, prefix='/roles', tags=['Roles'])
