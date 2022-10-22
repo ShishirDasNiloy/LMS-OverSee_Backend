71b5fd4c76b53654d5933a4887822d9d75fde559
ShishirDasNiloy <shishirdasniloy@gmail.com>
2022-10-22 09:58:40 +0600

    added: crete acces token and validate token

---
+++ src/utils/token.py
@@ -0,0 +1,37 @@
+from datetime import datetime, timedelta
+from typing import Optional
+from db import settings
+from exceptions import *
+from schemas.token import TokenData
+from jose import jwt, JWTError, ExpiredSignatureError
+
+
+class Token:
+    @staticmethod
+    def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
+        to_encode = data.copy()
+        if expires_delta:
+            expire = datetime.utcnow() + expires_delta
+        else:
+            expire = datetime.utcnow() + timedelta(days = 2)
+
+        to_encode.update({"exp": expire})
+        encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm = settings.ALGORITHM)
+        return encoded_jwt
+
+    @staticmethod
+    def validate_token(token: str) -> TokenData:
+        try:
+            payload = jwt.decode(token, settings.SECRET_KEY,algorithms = [settings.ALGORITHM], options = {"verify_sub": False})
+            user_id = payload.get("sub")
+
+            if user_id is None:
+                raise AppException.CredentialsException()
+            token_data = TokenData(user_id = user_id)
+            return token_data
+
+        except ExpiredSignatureError:
+            raise AppException.BadRequest({"message": "Token expired"})
+        except JWTError as error:
+            print(error)
+            raise AppException.CredentialsException()
