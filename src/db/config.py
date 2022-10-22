22187c51eb3313032c9ac78727c746bc104f8cea
ShishirDasNiloy <shishirdasniloy@gmail.com>
2022-10-22 09:42:46 +0600

    fixed: import problem

--- src/db/config.py
+++ src/db/config.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from core import settings
+from db.core import settings
 
 DATABASE_URL = settings.DATABASE_URL
 
