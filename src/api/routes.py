22f23779fb7addbf6997bb88b6a68d9cb80e2b5d
ShishirDasNiloy <shishirdasniloy@gmail.com>
2022-10-22 10:54:06 +0600

    added: route for admin api

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-from .endpoints import users, roles
+from .endpoints import users, roles, admin
 
 api_router = APIRouter()
 
 api_router.include_router(users.router, prefix='', tags = ["Users"])
 api_router.include_router(roles.router, prefix='/roles', tags=['Roles'])
+api_router.include_router(admin.router, prefix='/admin', tags=['Admin'])
