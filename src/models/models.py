a86251c6ceccc23eb40e3c887c1d04ca66084f04
ShishirDasNiloy <shishirdasniloy@gmail.com>
2022-12-11 17:25:50 +0600

    added: teacher table

--- src/models/models.py
+++ src/models/models.py
@@ -14,6 +14,8 @@ class User(BaseModel):
 
     role = relationship("Role", back_populates="user")
     user_details = relationship("UserDetail", back_populates="user")
+    teacher = relationship("Teacher", back_populates = "user_teacher")
+    student = relationship("Student", back_populates = "user_student")
 
 
 class Role(BaseModel):
@@ -23,7 +25,7 @@ class Role(BaseModel):
     user = relationship("User", back_populates = "role")
 
 
-class UserDetails(BaseModel):
+class UserDetail(BaseModel):
     __tablename__ = "user_details"
     user_id = Column(Integer, ForeignKey("users.id"))
     fathers_name = Column(String(100), nullable = True)
@@ -31,7 +33,25 @@ class UserDetails(BaseModel):
     dob = Column(Date, nullable=True)
     blood_group = Column(String(5), nullable=True)
 
+    user = relationship("User", back_populates="user_details")
+
 
 class LoginLog(BaseModel):
     __tablename__ = "login_log"
     user_id = Column(Integer, ForeignKey("users.id"))
+
+
+class Teacher(BaseModel):
+    __tablename__ = "teachers"
+    user_id = Column(Integer, ForeignKey("users.id"))
+    teacher_id = Column(String(20), nullable = True)
+    designation = Column(String(50), nullable = True)
+    degree = Column(String(255), nullable=True)
+    institute = Column(String(255), nullable = True)
+
+
+    user_teacher = relationship("User", back_populates = "teacher")
+
+
+    
+
