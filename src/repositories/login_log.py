e7dfc5ecd0b7cbabf2c49822dc00def8c42b3dd2
ShishirDasNiloy <shishirdasniloy@gmail.com>
2022-12-11 23:32:56 +0600

    removed: login log

--- src/repositories/login_log.py
+++
@@ -1,5 +0,0 @@
-from repositories import BaseRepo
-from schemas import LoginLogIn, LoginLogUpdate
-from models import LoginLog
-
-login_log_repo = BaseRepo[LoginLog, LoginLogIn, LoginLogUpdate](LoginLog)
