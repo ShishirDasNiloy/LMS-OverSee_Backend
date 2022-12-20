e01427cf9b187b08d2734b8ace6a95cefcadb3c8
ShishirDasNiloy <shishirdasniloy@gmail.com>
2022-12-20 22:05:25 +0600

    added: tecaher and student route

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
-from .endpoints import users, roles, admin
+from .endpoints import users, roles, admin, teacher, student
 
 api_router = APIRouter()
 
 api_router.include_router(users.router, prefix='', tags = ["Users"])
 api_router.include_router(roles.router, prefix='/roles', tags=['Roles'])
 api_router.include_router(admin.router, prefix='/admin', tags=['Admin'])
+api_router.include_router(teacher.router, prefix="/teacher", tags=['Teacher'])
+api_router.include_router(student.router, prefix="/student", tags=['Student'])
