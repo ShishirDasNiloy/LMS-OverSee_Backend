30762e6666bdc9c9516e1f7abb6938ecbf9ae6bc
ShishirDasNiloy <shishirdasniloy@gmail.com>
2022-12-20 22:08:01 +0600

    added: user active switcher and active inactive teacher list

--- src/api/endpoints/admin.py
+++ src/api/endpoints/admin.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, Depends
 from db import get_db
 from exceptions.service_result import handle_result
-from schemas import UserOut, UserCreateWitoutRole
+from schemas import UserOut, UserCreateWitoutRole, ResultInt
 from sqlalchemy.orm import Session
 from services import admin_service
+from typing import List, Union
 
 
 router = APIRouter()
@@ -13,3 +14,34 @@ router = APIRouter()
 def signup(data_in: UserCreateWitoutRole, db: Session = Depends(get_db)):
     admn = admin_service.signup_admin(db, data_in=data_in)
     return handle_result(admn)
+
+
+
+@router.patch('/switch/active/{id}', response_model=UserOut)
+def user_active_switcher(id: int, db: Session = Depends(get_db)):
+    act = admin_service.user_active_switcher(db=db, id=id)
+    return handle_result(act)
+
+
+@router.get('/active-teacher', response_model=List[Union[ResultInt, List[UserOut]]])
+def active_teachers(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    at = admin_service.active_teachers(db=db, skip=skip, limit=limit)
+    return handle_result(at)
+
+
+@router.get('/inactive-teacher', response_model=List[Union[ResultInt, List[UserOut]]])
+def inactive_teachers(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    iat = admin_service.inactive_teachers(db=db, skip=skip, limit=limit)
+    return handle_result(iat)
+
+
+@router.get('/active-student', response_model=List[Union[ResultInt, List[UserOut]]])
+def active_students(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    at = admin_service.active_students(db=db, skip=skip, limit=limit)
+    return handle_result(at)
+
+
+@router.get('/inactive-student', response_model=List[Union[ResultInt, List[UserOut]]])
+def inactive_students(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    iat = admin_service.inactive_students(db=db, skip=skip, limit=limit)
+    return handle_result(iat)
