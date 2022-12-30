48030d9ab7fcd93858979c23e69fe2797fdf1483
ShishirDasNiloy <shishirdasniloy@gmail.com>
2022-12-30 13:21:19 +0600

    added: teacher apis

--- src/api/endpoints/teacher.py
+++ src/api/endpoints/teacher.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter, Depends
 from db import get_db
 from exceptions.service_result import handle_result
-from schemas import TeacherSignup, TeacherOut, UserLogin
+from schemas import TeacherSignup, TeacherOut, UserLogin, ClassRoomCreate
 from sqlalchemy.orm import Session
-from services import teacher_service
+from services import teacher_service, class_room_service
 from typing import List
 from schemas import Token
+from api.auth_dependcies import logged_in_teacher
 
 router = APIRouter()
 
@@ -25,3 +26,14 @@ def get_teacher(db: Session = Depends(get_db)):
 def login(data_in: UserLogin, db: Session = Depends(get_db)):
     user = teacher_service.login(db, data_in.identifier, data_in.password)
     return handle_result(user)
+
+
+@router.post('/create-classroom')
+def create_class(data_in: ClassRoomCreate, db: Session = Depends(get_db), current_user: Session = Depends(logged_in_teacher)):
+    cc = class_room_service.create_classroom_teacher(db=db, data_in=data_in, user_id=current_user.id)
+    return handle_result(cc)
+
+@router.get('all-classroom')
+def get_all_classroom(teacher_id: int, skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    gac = class_room_service.classroom_by_teacher_id(db=db, teacher_id=teacher_id, skip=skip, limit=limit)
+    return gac
