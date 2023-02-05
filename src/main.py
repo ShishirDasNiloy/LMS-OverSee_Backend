a0d76a665a7815c9a1931305b48872b27d9b5343
ShishirDasNiloy <shishirdasniloy@gmail.com>
2023-02-05 21:00:55 +0600

    added: static file paths and exception handlers

--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
 import os
 import uvicorn
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, status
 import api.routes
 from db import settings
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from exceptions import AppExceptionCase, AppException, app_exception_handler
+from exceptions import AppExceptionCase, AppException, app_exception_handler, generic_exception_handler
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.exceptions import RequestValidationError, ValidationError
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 
 app = FastAPI(title = 'OverSee')
 
@@ -26,9 +29,41 @@ def custom_app_exception_handler(request: Request, exc: AppException):
     return app_exception_handler(request, exc)
 
 
+@app.exception_handler(RequestValidationError)
+def request_validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content=jsonable_encoder(
+            {
+                "detail": exc.errors(),
+                "body": exc.body,
+                "your_additional_errors": {
+                    "Will be": "Inside",
+                    "This": " Error message",
+                },
+            }
+        ),
+    )
+
+
+@app.exception_handler(ValidationError)
+def validation_exception_handler(request: Request, exc: ValidationError):
+    print(exc)
+    return app_exception_handler(request, AppException.BadRequest(exc))
+
+
+@app.exception_handler(Exception)
+def custom_generic_exception_handler(request: Request, exc: Exception):
+    print(exc)
+    return generic_exception_handler(request, exc)
+
+
 parent_path = os.path.dirname(os.path.realpath(__file__))
 
 app.mount('/static/', StaticFiles(directory=f'{parent_path}/static'), name='static')
+app.mount('/api/images/', StaticFiles(directory=f'{parent_path}/assets/img'), name='img')
+app.mount('/api/pdf/', StaticFiles(directory=f'{parent_path}/assets/pdf'), name='pdf')
+
 
 templates = Jinja2Templates(directory='templates')
 
