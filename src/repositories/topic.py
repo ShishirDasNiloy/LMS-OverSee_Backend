ac71c45299162ffe1d9ecea2076869caee3dea61
ShishirDasNiloy <shishirdasniloy@gmail.com>
2023-02-05 21:10:37 +0600

    added: get topic by class

--- src/repositories/topic.py
+++ src/repositories/topic.py
@@ -1,10 +1,15 @@
 from models import Topic
 from schemas import TopicIn, TopicUpdate
 from repositories import BaseRepo
+from sqlalchemy.orm import Session
+from sqlalchemy.sql import desc
 
 
 class TopicRepo(BaseRepo[Topic, TopicIn, TopicUpdate]):
-    pass
+    
+    def get_topic_by_class(self, db: Session, class_room_id: int):
+        data = db.query(self.model).filter(self.model.class_room_id == class_room_id).order_by(desc(self.model.id)).all()
+        return data
 
 
 topic_repo = TopicRepo(Topic)
