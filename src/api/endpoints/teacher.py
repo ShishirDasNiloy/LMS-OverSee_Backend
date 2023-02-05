24e10ee1d0208c8470135404c3c992ee61516d52
ShishirDasNiloy <shishirdasniloy@gmail.com>
2023-02-05 21:07:19 +0600

    added: class create and topic create apis

--- src/api/endpoints/teacher.py
+++ src/api/endpoints/teacher.py
@@ -1,12 +1,14 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, UploadFile, File
 from db import get_db
 from exceptions.service_result import handle_result
-from schemas import TeacherSignup, TeacherOut, UserLogin, ClassRoomCreate
+from schemas import TeacherSignup, TeacherOut, UserLogin, ClassRoomCreate, TopicCreate, TopicIn
 from sqlalchemy.orm import Session
-from services import teacher_service, class_room_service
+from services import teacher_service, class_room_service, topic_service
 from typing import List
 from schemas import Token
-from api.auth_dependcies import logged_in_teacher
+from api.auth_dependcies import logged_in_teacher, logged_in
+from utils import UploadFileUtils
+from typing import Optional
 
 router = APIRouter()
 
@@ -33,7 +35,46 @@ def create_class(data_in: ClassRoomCreate, db: Session = Depends(get_db), curren
     cc = class_room_service.create_classroom_teacher(db=db, data_in=data_in, user_id=current_user.id)
     return handle_result(cc)
 
+@router.get('/classroom_by_user/')
+def get_classroom(db: Session = Depends(get_db), current_user: Session = Depends(logged_in_teacher)):
+    get_class = class_room_service.classroom_by_teacher(db=db, user_id=current_user.id)
+    return get_class
+
 @router.get('all-classroom')
 def get_all_classroom(teacher_id: int, skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     gac = class_room_service.classroom_by_teacher_id(db=db, teacher_id=teacher_id, skip=skip, limit=limit)
     return gac
+
+@router.get('/classes')
+def get_all(db: Session = Depends(get_db)):
+    get = class_room_service.get(db=db)
+    return handle_result(get)
+
+
+@router.post('/create-topic')
+async def create_topic(data_in: TopicCreate, db: Session = Depends(get_db), current_user: Session = Depends(logged_in_teacher)):
+    ct = topic_service.create_topic(db=db, data_in = data_in, user_id=current_user.id)
+    return ct
+
+@router.get('/topic-by-class')
+def get_all(class_room_id: int, db: Session = Depends(get_db)):
+    gt = topic_service.get_topic_by_class(db=db, class_room_id=class_room_id)
+    return gt
+
+
+@router.post('/create_topic-with-file')
+async def upload_file(class_room_id: int, topic_name: str, topic_type: Optional[str] = None, topic_details: Optional[str] = None, file: Optional[UploadFile] = File(None), db: Session = Depends(get_db), current_user: Session = Depends(logged_in)):
+
+    if file is None:
+        new_file_name = ''
+    else:
+        up_img = UploadFileUtils(file=file)
+
+    # prefix is the short service name
+        new_file_name = up_img.upload_files(prefix='topic', path='./assets/files', accept_extensions=['jpg', 'jpeg', 'png', 'pdf'])
+
+
+    # save in db
+    file_in_db = topic_service.create(db=db, data_in=TopicIn(class_room_id=class_room_id, created_by=current_user.id, topic_name=topic_name, topic_type=topic_type, topic_details=topic_details, image_pdf_string=new_file_name))
+
+    return handle_result(file_in_db)
