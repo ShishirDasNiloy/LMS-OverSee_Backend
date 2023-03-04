5995a1bb24e2d88b4d491d0770e9a26aafc41e30
ShishirDasNiloy <shishirdasniloy@gmail.com>
2023-03-04 12:19:14 +0600

    added: active users

--- src/repositories/student.py
+++ src/repositories/student.py
@@ -1,10 +1,32 @@
-from models import Student
+from models import Student, User
 from schemas import StudentIn, StudentUpdate
 from repositories import BaseRepo
+from sqlalchemy.orm import Session
 
 
 class StudentRepo(BaseRepo[Student, StudentIn, StudentUpdate]):
-    pass
+    def active_students(self, db: Session, skip: int, limit: int):
+        
+        data = db.query(self.model).join(User, User.id == self.model.user_id).filter(User.is_active == True).filter(
+            User.role_id == 3).offset(skip).limit(limit).all()
+        return data
+    
+    def active_students_count(self, db: Session):
+        data_count = db.query(User).filter(
+            User.is_active == True).filter(User.role_id == 3).all()
+        return {"results": len(data_count)}
+    
+
+    def inactive_students(self, db: Session, skip: int, limit: int):
+        
+        data = db.query(self.model).join(User, User.id == self.model.user_id).filter(User.is_active == False).filter(
+            User.role_id == 3).offset(skip).limit(limit).all()
+        return data
+    
+    def inactive_students_count(self, db: Session):
+        data_count = db.query(User).filter(
+            User.is_active == False).filter(User.role_id == 3).all()
+        return {"results": len(data_count)}
 
 
 student_repo = StudentRepo(Student)
--- src/repositories/teacher.py
+++ src/repositories/teacher.py
@@ -1,9 +1,31 @@
-from models import Teacher
+from models import Teacher, User
 from repositories import BaseRepo
 from schemas import TeacherIn, TeacherUpdate
+from sqlalchemy.orm import Session
 
 
 class TeacherRepo(BaseRepo[Teacher, TeacherIn, TeacherUpdate]):
-    pass
+    def active_teachers(self, db: Session, skip: int, limit: int):
+        
+        data = db.query(self.model).join(User, User.id == self.model.user_id).filter(User.is_active == True).filter(
+            User.role_id == 2).offset(skip).limit(limit).all()
+        return data
+    
+    def active_teacher_count(self, db: Session):
+        data_count = db.query(User).filter(
+            User.is_active == True).filter(User.role_id == 2).all()
+        return {"results": len(data_count)}
+    
+
+    def inactive_teachers(self, db: Session, skip: int, limit: int):
+        
+        data = db.query(self.model).join(User, User.id == self.model.user_id).filter(User.is_active == False).filter(
+            User.role_id == 2).offset(skip).limit(limit).all()
+        return data
+    
+    def inactive_teacher_count(self, db: Session):
+        data_count = db.query(User).filter(
+            User.is_active == False).filter(User.role_id == 2).all()
+        return {"results": len(data_count)}
 
 teacher_repo = TeacherRepo(Teacher)
