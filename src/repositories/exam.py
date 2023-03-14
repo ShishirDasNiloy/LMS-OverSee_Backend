91f6c1bf0e66da214a1fe92342e9cccdf55f8c37
ShishirDasNiloy <shishirdasniloy@gmail.com>
2023-03-15 00:05:44 +0600

    added: exam by student

--- src/repositories/exam.py
+++ src/repositories/exam.py
@@ -1,5 +1,5 @@
 from repositories import BaseRepo
-from models import Exam
+from models import Exam, ExamWithStudent
 from schemas import ExamIn, ExamUpdate
 from sqlalchemy.orm import Session
 from sqlalchemy import desc
@@ -9,6 +9,10 @@ from sqlalchemy import desc
 class ExamRepo(BaseRepo[Exam, ExamIn, ExamUpdate]):
     def exam_by_teacher(self, db: Session, user_id: int):
         data = db.query(self.model).filter(self.model.user_id == user_id).order_by(desc(self.model.created_at)).all()
+        return data
+    
+    def exam_by_student_id(self, db: Session, student_id: int):
+        data = db.query(self.model).join(ExamWithStudent, ExamWithStudent.exam_id == self.model.id).filter(ExamWithStudent.user_id == student_id).order_by(desc(ExamWithStudent.id)).all()
         return data
 
 
